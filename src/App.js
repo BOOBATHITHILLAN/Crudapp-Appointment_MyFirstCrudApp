4d3fe645b74b2e48a66749e311487ba713fb817b
BOOBATHITHILLAN <boobathithillan@gmail.com>
2023-06-20 17:20:05 +0530

    React crud

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import "./App.css";
+import { Home } from "./Components/Home";
+import { useState } from "react";
+import { About } from "./Components/About";
+import { CreateAppointment } from "./Components/CreateAppointment";
+import { ReadAppointment } from "./Components/ReadAppointment";
+import { DeleteAppointment } from "./Components/DeleteAppointment";
+import { UpdateAppointment } from "./Components/UpdateAppointment";
+import Header from "./Components/Header";
+import Navbar from "./Components/Navbar";
+
+
+
+function App() {
+
+
+  
+  const [create, setCreate] = useState([
+    {
+      Id: 1,
+      ToMeet: "Boobathi",
+      Date: "2023-02-09",
+      Time: "10:00",
+      Purpose: "Medical Checkup",
+    },
+  ]);
+
+  return (
+
+
+    <div className="App container">
+      
+      <Header text="CRUD APPLICATION"/>
+
+      <Router>
+
+        <Navbar  Link={Link} text1="Home" text2="Create-Appointment"  text3="Read-Appointment"  text4="Update-Appointment" text5="Delete-Appointment" text6="About"/>
+        
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/Create-Appointment"
+            element={
+              <CreateAppointment create={create} setCreate={setCreate} />
+            }
+          />
+          <Route
+            path="/Read-Appointment"
+            element={<ReadAppointment create={create} />}
+          />
+          <Route
+            path="/Update-Appointment"
+            element={
+              <UpdateAppointment create={create} setCreate={setCreate} />
+            }
+          />
+          <Route
+            path="/Delete-Appointment"
+            element={
+              <DeleteAppointment create={create} setCreate={setCreate} />
+            }
+          />
+          <Route path="/About" element={<About />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
+
+
---
+++ src/Components/About.js
@@ -0,0 +1,9 @@
+export function About() {
+  return (
+    <div className="bg-primary bg-gradient p-4 text-white">
+      <p className="fw-bold p-4">CRUD means Create, Read, Update,Delete.</p>
+      <p className="fw-bold p-4">This CRUD app is created by Boobathi Thillan</p>
+      <p className="fw-bold p-4">Copy right @ Boobathi 2023</p>
+    </div>
+  );
+}
